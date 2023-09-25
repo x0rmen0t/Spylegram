dde291ffd839076e53a9c2e76f209e920a6a78c2
annabandicoot <annabandicoot86@gmail.com>
2023-09-25 18:51:16 +0200

    Update README.md

--- README.md
+++ README.md
@@ -1,2 +1,42 @@
 # Spylegram
 Telegram Scraper for Hacktivist Channels and Groups
+
+This Python script empowers you to explore and scrape Telegram's vast universe of hacktivist channels and groups with ease. Built on the Telethon library, this Telegram scraper is designed to collect messages, photos, and documents from your favorite hacktivist channels and groups on Telegram. Whether you're an activist, journalist, or researcher, this tool provides a powerful means to access and archive valuable information from Telegram's dynamic communities.
+
+## Features
+- **Channel Scraping**: Retrieve messages, photos, and documents from Telegram hacktivist channels.
+
+- **Data Organization**: Automatically organize and save scraped data into sqlite database for easy analysis and reference.
+
+- **Real-time Updates**: Keep up to date with the latest content by periodically scraping channels and groups of interest.
+
+- **Customizable Scraping**: Configure the script to scrape specific channels or groups, target particular message types, and set scraping intervals.
+
+
+## Signing In
+
+Before working with Telegram’s API, you need to get your own API ID and hash:
+
+    [Login](https://my.telegram.org/auth) to your Telegram account with the phone number of the developer account to use.
+    Click under API Development tools.
+    A Create new application window will appear. Fill in your application details. There is no need to enter any URL, and only the first two fields (App title and Short name) can currently be changed later.
+    Click on Create application at the end. Remember that your API hash is secret and Telegram won’t let you revoke it. Don’t post it anywhere!
+
+
+## How to Use:
+
+    Clone the repository to your local machine.
+    Install the necessary dependencies.
+    Configure the script with your Telegram API credentials.
+    Customize scraping settings to target the channels and groups you're interested in.
+    Run the script to start scraping Telegram hacktivist channels and groups.
+
+❗Please note that this script should be used responsibly and in compliance with Telegram's terms of service and community guidelines.
+
+
+
+
+
+## Feedback
+
+If you have any feedback, please reach out to me at ....
---
+++ src/message.py
@@ -0,0 +1,114 @@
+from dataclasses import dataclass
+from datetime import datetime
+from typing import Optional, List, Tuple, Union
+from telethon.errors import ChannelPrivateError
+from telethon import TelegramClient
+from telethon.tl.types import MessageEntityTextUrl, PeerChannel, Message, MessageService, MessageEntityUnknown, MessageEntityUrl
+
+
+import logging
+
+logging.basicConfig(format='[%(levelname) 5s/%(asctime)s] %(name)s: %(message)s',
+                    level=logging.INFO)
+
+TypeMessageEntity = Union[MessageEntityUnknown, MessageEntityUrl]
+
+
+@dataclass
+class MessageData:
+    message_id: int
+    channel_id: int
+    channel_name: str
+    message_date: Optional[datetime] = None
+    message_pinned: bool = False
+    message_views: int = 0
+    message_forwards: int = 0
+    message_media: bool = False
+    message_text: Optional[str] = None
+    message_edit_date: Optional[datetime] = None
+    url_in_message: Optional[str] = None
+    message_fwd_from: bool = False
+    message_fwd_from_date: Optional[datetime] = None
+    message_fwd_from_channel_id: Optional[int] = None
+    message_fwd_from_channel_username: Optional[str] = None
+    message_fwd_from_channel_link: Optional[str] = None
+
+
+async def get_first_message_date(client: TelegramClient, channel_url: str) -> datetime | str:
+    """We count channel creation date by the 1st service message posted in the channel;
+        If None then we take creation date from channel entity
+    """
+    async for message in client.iter_messages(channel_url, reverse=True, limit=1):
+        if isinstance(message, MessageService):
+            return message.date
+        else:
+            return ""
+
+
+async def get_last_message_id(client: TelegramClient, channel: str) -> int:
+    async for message in client.iter_messages(channel, limit=1):
+        return message.id
+
+
+def get_url(message_entity: Optional[List['TypeMessageEntity']]) -> str:
+    for entity in message_entity:
+        if isinstance(entity, MessageEntityTextUrl):
+            return entity.url
+
+
+def get_telegram_link(fwd_channel_username: str) -> str:
+    return f"https://t.me/{fwd_channel_username}"
+
+
+async def get_fwd_channel_username(client: TelegramClient, message: Message) -> Tuple[str, str] | Tuple[None, None]:
+    if isinstance(message.fwd_from.from_id, PeerChannel):
+        try:
+            entity = await client.get_entity(message.fwd_from.from_id.channel_id)
+            tg_link = get_telegram_link(entity.username)
+            return entity.username, tg_link
+        except ChannelPrivateError as e:
+            logging.warning(
+                "Cant get information about the channel due to access restrictions. Channe might be marked as private",
+                e)
+            return None, None
+    else:
+        return message.fwd_from.from_name, message.fwd_from.from_id
+
+
+async def get_message(client: TelegramClient, *args, **kwargs):
+    return await client.get_messages(*args, **kwargs)
+
+
+def create_message_data(message: Message, channel_id: int, channel_username: str, fwd_from_channel_username: str,
+                        tg_link: str) -> MessageData:
+    """
+    Create a MessageData object from a Telegram message.
+
+    Args:
+        message (Message obj): The Telegram message to create data from.
+        channel_id (int): The ID of the Telegram channel.
+        channel_username (str): The username of the Telegram channel.
+        fwd_from_channel_username (str): The username of the forwarded channel (if any).
+        tg_link (str): The link to the forwarded channel (if any).
+
+    Returns:
+        MessageData: A MessageData object representing the message.
+    """
+    return MessageData(
+        message_id=message.id,
+        channel_id=channel_id,
+        channel_name=channel_username,
+        message_date=message.date,
+        message_text=message.text,
+        message_pinned=message.pinned,
+        message_fwd_from=bool(message.fwd_from),
+        message_fwd_from_date=message.fwd_from.date if message.fwd_from else None,
+        message_fwd_from_channel_id=message.fwd_from.from_id.channel_id if message.fwd_from and message.fwd_from.from_id else None,
+        message_fwd_from_channel_username=fwd_from_channel_username,
+        message_edit_date=message.edit_date if message.edit_date else None,
+        message_views=message.views,
+        message_forwards=message.forwards,
+        message_media=bool(message.media),
+        url_in_message=get_url(message.entities) if message.entities else None,
+        message_fwd_from_channel_link=tg_link if message.fwd_from else None
+    )
